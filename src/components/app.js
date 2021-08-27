bf3d492b3b331eeb898e2af54d29590c4659db80
WanShahrizan <shahrizan_98@yahoo.com>
2021-08-27 21:39:38 +0800

    Finished chat app

---
+++ src/components/app.js
@@ -0,0 +1,38 @@
+const { useState, useEffect } = require("react");
+const SignUp = require("./signup");
+const Chat = require("./chat");
+const supabase = require("../utils/supabase");
+
+function App() {
+  const [session, setSession] = useState(null);
+  useEffect(function () {
+    setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange(function (event, supaSession) {
+      setSession(supaSession);
+    });
+  }, []);
+  console.log(session);
+
+  let markup = <SignUp />;
+  if (session) {
+    console.clear();
+    console.log(session.user.email);
+
+    //If you are logged in(meaning there is a section)
+    markup = (
+      <div>
+        <h2>You are logged In as {session.user.email}</h2>
+        <Chat />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Supabase + React Chat App</h2>
+      {markup}
+    </div>
+  );
+}
+
+module.exports = App;
